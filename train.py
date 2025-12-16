eb9e76503b60eb8539565c0dd737a8f16e888893
Tom Pollak <tompollak1000@gmail.com>
2025-12-16 19:27:25 +0100

    x

--- train.py
+++ train.py
@@ -67,6 +67,7 @@ def main(conf: OnPolicyKDConfig = OnPolicyKDConfig()) -> None:
 
     # Tokenizer
     tokenizer = AutoTokenizer.from_pretrained(conf.model_name, use_fast=True)
+    tokenizer.padding_side = "left"
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
 
